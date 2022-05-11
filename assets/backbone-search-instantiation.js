d5b1e9326f93072f9caac28f14540d4cf9724873
StephenMLF <stephen@marshmallowlaserfeast.com>
2022-05-11 15:49:35 +0100

    html side input trigger

--- assets/backbone-search-instantiation.js
+++ assets/backbone-search-instantiation.js
@@ -2,11 +2,25 @@
 
 posts.fetch();
 
-$('.some-search-input').on('change', function() {
+$('.some-search-input').on('change', function () {
     var results = posts.filter($(this).val().trim()),
         $resultsContainer = $('.search-results').empty();
 
-    _(results).each(function(r) {
+    _(results).each(function (r) {
         $resultsContainer.append(new SearchResult({model: r}).render().$el);
     });
 });
+
+
+function doSearch() {
+    
+    var searchString = document.getElementById("some-search-input").value;
+
+    var results = posts.filter(searchString.trim()),
+        $resultsContainer = document.getElementById("search-result").empty();
+
+    _(results).each(function (r) {
+        $resultsContainer.append(new SearchResult({model: r}).render().$el);
+    });
+    
+}
